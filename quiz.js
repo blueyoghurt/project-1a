76a1f3b4691ebe8b791f05e9fadc349e6ba6d9ff
Corine Lim <corinelim@Corine-Lims-MacBook-Pro.local>
2016-08-28 12:01:05 +0800

    updates from mac

--- quiz.js
+++ quiz.js
@@ -42,12 +42,65 @@ document.addEventListener('DOMContentLoaded',function(){
   quiz.currentQuestion = currentQuestion(0, (numberOfQuestions()-1));
   numberOfAnswers(quiz.currentQuestion);
   populateQuestion();
-});
 
-function numberOfQuestions (){
-  // console.log('number of questions', quiz.questions.length);
-  return quiz.questions.length;
-}
+  function playTurn(){
+    if (this.value == correctAnswer(quiz.currentQuestion)){
+      document.getElementById('CorrectAnswer').className = 'container alert alert-success'
+      document.getElementById('WrongAnswer').className = 'container alert alert-danger hidden'
+      return true;
+    }
+    document.getElementById('WrongAnswer').className = 'container alert alert-danger'
+    document.getElementById('CorrectAnswer').className = 'container alert alert-success hidden'
+    return false;
+  }
+
+  function isGameOver(){
+    var questionsAsked = 0;
+    for (i=0; i < quiz.questions.length; i++){
+      if (quiz.questionsAskedIndex[i] == 1){
+        questionsAsked += 1;
+      }
+    }
+    if (questionsAsked == quiz.questions.length){
+      return true;
+    }
+    else
+    console.log(questionsAsked);
+    return false;
+  }
+
+  function restart(){
+    console.log('reset pressed');
+    quiz.player1Points = quiz.player2Points = 0;
+    quiz.isGameOver = false,
+    quiz.questionsAskedIndex = [];
+    for (i = 0; i < quiz.questions.length; i++ ){
+      quiz.questionsAskedIndex[i] = 0;
+    }
+    document.getElementById('WrongAnswer').className = 'container alert alert-danger hidden'
+    document.getElementById('CorrectAnswer').className = 'container alert alert-success hidden'
+  }
+
+  // Populate the question and options for currentQuestion
+  function populateQuestion(){
+    var OptionsAvailable = document.getElementsByClassName('quiz-ans');
+    for (i=0 ; i< quiz.questions[quiz.currentQuestion].options.length; i++){
+      OptionsAvailable[i].textContent = quiz.questions[quiz.currentQuestion].options[i];
+    }
+    var question = document.getElementById('QuestionText');
+    question.textContent = quiz.questions[quiz.currentQuestion].prompt;
+    //  document.getElementById('QuestionNumber').textContent = 'Qn' + ;
+  }
+
+  progress(80, $('#progressBar'));
+
+  function progress(percent, $element) {
+    var progressBarWidth = percent * $element.width() / 100;
+    console.log('progressbar loaded');
+    $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + '% ');
+  }
+
+}); //DOMContentLoaded
 
 function currentQuestion (min,max){
   currentQ = Math.floor(min + Math.random() * ((max-min) +1));
@@ -56,42 +109,6 @@ function currentQuestion (min,max){
   } console.log('Current question is:', currentQ);
   return currentQ;
 }
-
-function correctAnswer(index){
-  console.log('Correct answer is', quiz.questions[index].correctAnswerIndex);
-  return quiz.questions[index].correctAnswerIndex;
-}
-
-function numberOfAnswers(index){
-  console.log('Number of options are', quiz.questions[index].options.length);
-  return quiz.questions[index].options.length;
-}
-
-function playTurn(){
-  if (this.value == correctAnswer(quiz.currentQuestion)){
-    document.getElementById('CorrectAnswer').className = 'container alert alert-success'
-    document.getElementById('WrongAnswer').className = 'container alert alert-danger hidden'
-    return true;
-  }
-  document.getElementById('WrongAnswer').className = 'container alert alert-danger'
-  document.getElementById('CorrectAnswer').className = 'container alert alert-success hidden'
-  return false;
-}
-
-// function isGameOver(){
-// var questionsAsked = 0;
-// for (i=0; i < quiz.questions.length; i++){
-//   if (quiz.questionsAskedIndex[i] == 1){
-//     questionsAsked += 1;
-//   }
-// }
-// if (questionsAsked == quiz.questions.length){
-//   return true;
-// }
-// else
-// console.log(questionsAsked);
-// return false;
-// }
 
 function whoWon(){
   if (isGameOver()){
@@ -103,35 +120,17 @@ function whoWon(){
   } else return 0;
 }
 
-function restart(){
-  console.log('reset pressed');
-  quiz.player1Points = quiz.player2Points = 0;
-  quiz.isGameOver = false,
-  quiz.questionsAskedIndex = [];
-  for (i = 0; i < quiz.questions.length; i++ ){
-    quiz.questionsAskedIndex[i] = 0;
-  }
-  for (i=0; i< 2; i++){
-    console.log('in for loop');
-    document.getElementsByClassName('alert')[i].className += ' hidden';
-  }
+function numberOfQuestions (){
+  // console.log('number of questions', quiz.questions.length);
+  return quiz.questions.length;
 }
 
-// Populate the question and options for currentQuestion
-function populateQuestion(){
-  var OptionsAvailable = document.getElementsByClassName('quiz-ans');
-  for (i=0 ; i< quiz.questions[quiz.currentQuestion].options.length; i++){
-    OptionsAvailable[i].textContent = quiz.questions[quiz.currentQuestion].options[i];
-  }
-  var question = document.getElementById('QuestionText');
-  question.textContent = quiz.questions[quiz.currentQuestion].prompt;
-  //  document.getElementById('QuestionNumber').textContent = 'Qn' + ;
+function correctAnswer(index){
+  console.log('Correct answer is', quiz.questions[index].correctAnswerIndex);
+  return quiz.questions[index].correctAnswerIndex;
 }
 
-progress(80, $('#progressBar'));
-
-function progress(percent, $element) {
-  var progressBarWidth = percent * $element.width() / 100;
-  console.log('progressbar loaded');
-  $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + '% ');
+function numberOfAnswers(index){
+  console.log('Number of options are', quiz.questions[index].options.length);
+  return quiz.questions[index].options.length;
 }
